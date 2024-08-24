ed32e8eb500b78466febaa4067146361a8e42a7c
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2024-08-24 18:03:38 +0100

    hmm

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get update && apt-get install -y wget unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
